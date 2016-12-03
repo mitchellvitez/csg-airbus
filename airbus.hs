cf9a6ec6f6283ba6ed1f046326c569c7a06e4d4d
Mitchell Vitez <mitchell@vitez.me>
2016-12-02 23:11:46 -0500

    Clean up long lines

--- airbus.hs
+++ airbus.hs
@@ -24,7 +24,8 @@ data Season = Season [SeasonVar] [Day] deriving Show
 data SeasonVar = SeasonVar Text Text deriving Show
 data Day = Day Date [Trip] deriving Show
 data Date = Date Text Text Text deriving Show
-data Trip = EastboundTrip Int Int Time Time Time Time | WestboundTrip Int Time Time Time deriving Show
+data Trip = EastboundTrip Int Int Time Time Time Time 
+  | WestboundTrip Int Time Time Time deriving Show
 data Direction = Westbound | Eastbound
 data Time = Time Int Int Char deriving Show
 data SeasonRecord = 
@@ -39,7 +40,9 @@ parseSeason :: Parser Season
 parseSeason = do
   seasonVars <- many' parseSeasonVar
   many' endOfLine
-  days <- many' $ parseDay (if T.toLower (getSeasonVar "direction" seasonVars) == "westbound" then Westbound else Eastbound)
+  days <- many' $ parseDay 
+    (if T.toLower (getSeasonVar "direction" seasonVars) == "westbound" 
+    then Westbound else Eastbound)
   return $ Season seasonVars days
 
 parseSeasonVar :: Parser SeasonVar
@@ -50,11 +53,6 @@ parseSeasonVar = do
   many' endOfLine
   return $ SeasonVar (T.pack key) (T.pack val)
 
-{- parseSeasonRecord :: Parser SeasonRecord -}
-{- parseSeasonRecord = do -}
-  {- return SeasonRecord {  } -}
-  {- go from map to record -}
-  
 parseDate :: Parser Date
 parseDate = do
   y  <- count 4 digit
@@ -103,13 +101,13 @@ parseTrip Eastbound = do
   airport <- parseTime
   many' $ notChar '\n'
   many' endOfLine
-  return $ EastboundTrip (read tripNumber) (read blockNumber) bursley hill state airport 
+  return $ EastboundTrip (read tripNumber) (read blockNumber) 
+    bursley hill state airport 
 
 -- CONVERSION --
 
 showDate (Date y m d) = y <> "-" <> m <> "-" <> d
 
--- ??? Something better than "d - 1" for getting the day before. date/time library?
 sixPmDayBefore :: Date -> Text
 sixPmDayBefore (Date y m d) = y <> "-" <> m <> "-" <> day <> " 18:00:00"
   where day = (T.pack . show . subtract 1 . read . T.unpack) d
@@ -120,13 +118,28 @@ show2 n | T.length (T.pack (show n)) == 1 = "0" <> (T.pack (show n))
 
 tts :: Time -> Text
 tts (Time h m ampm) =
-  T.pack (show (if ampm == 'a' || h >= 12 then h else h + 12)) <> ":" <> show2 m 
+  T.pack (show (if ampm == 'a' || h >= 12 then h else h + 12))
+    <> ":" <> show2 m 
 
 tripToSql :: SeasonRecord -> Date -> Trip -> Text
 tripToSql season date (WestboundTrip tripNumber north mcnamara annArbor) =
-  "INSERT INTO `orms_westbound`(`number`, `date`, `north`, `mcnamara`, `annarbor`, `seasonId`) VALUES (" <> T.pack (show tripNumber) <> ",\"" <> showDate date <> "\",\"" <> tts north <> "\",\"" <> tts mcnamara <> "\",\"" <> tts annArbor <> "\"," <> seasonId season <> ");"
-tripToSql season date (EastboundTrip tripNumber blockNumber bursley hill state airport) =
-  "INSERT INTO `orms_trips`(`id`, `seasonId`, `number`, `direction`, `date`, `capacity`, `blockNumber`, `notes`, `reservationsOpen`, `reservationsClose`, `bursley`, `hill`, `state`, `airport`) VALUES (NULL," <> seasonId season <> "," <> T.pack (show tripNumber) <> ",\"" <> direction season <> "\",\"" <> showDate date <> "\"," <> T.pack (show 48) <> "," <> T.pack (show blockNumber) <> "," <> "\"\"" <> ",\"" <> reservationsOpen season <> "\",\"" <> sixPmDayBefore date <> "\",\"" <> tts bursley <> "\",\"" <> tts hill <> "\",\"" <> tts state <> "\",\"" <> tts airport <> "\");"
+  "INSERT INTO `orms_westbound`" <> 
+    "(`number`, `date`, `north`, `mcnamara`, `annarbor`, `seasonId`) VALUES (" <>
+    T.pack (show tripNumber) <> ",\"" <> showDate date <> "\",\"" <> tts north <>
+    "\",\"" <> tts mcnamara <> "\",\"" <> tts annArbor <> "\"," <>
+    seasonId season <> ");"
+tripToSql season date (EastboundTrip tripNumber blockNumber
+    bursley hill state airport) =
+  "INSERT INTO `orms_trips`" <> 
+  "(`id`, `seasonId`, `number`, `direction`, `date`, `capacity`, `blockNumber`," <>
+  " `notes`, `reservationsOpen`, `reservationsClose`, `bursley`, `hill`," <>
+  " `state`, `airport`) VALUES (NULL," <> seasonId season <> "," <>
+  T.pack (show tripNumber) <> ",\"" <> direction season <> "\",\"" <>
+  showDate date <> "\"," <> T.pack (show 48) <> "," <>
+  T.pack (show blockNumber) <> "," <> "\"\"" <> ",\"" <>
+  reservationsOpen season <> "\",\"" <> sixPmDayBefore date <> "\",\"" <>
+  tts bursley <> "\",\"" <> tts hill <> "\",\"" <> tts state <> "\",\"" <>
+  tts airport <> "\");"
 
 dayToSql :: SeasonRecord -> Day -> [Text]
 dayToSql sr (Day date trips) = map (tripToSql sr date) trips
@@ -137,7 +150,6 @@ toSql (Right (Season seasonVars days)) =
   let sr = seasonVarsToRecord seasonVars
   in concat $ map (dayToSql sr) days
 
--- ??? How to make parser read data to a record directly
 getSeasonVar :: Text -> [SeasonVar] -> Text
 getSeasonVar s lst = val seasonVar
   where pred (SeasonVar k _) = s == k
